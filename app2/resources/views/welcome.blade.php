25bc4237b0d2afec7d7fb17e888ac602f9bc0ce0
atsushi.isai <atsushi_i@branch-plus.co.jp>
2021-10-11 10:17:53 +0900

    add

--- app2/resources/views/welcome.blade.php
+++ app2/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel2
+                    Laravel
                 </div>
 
                 <div class="links">
